f99257aad52bb08f6855f317011fd5a6d99246f6
Lee Briggs <lee@leebriggs.co.uk>
2020-07-21 13:06:38 -0700

    multiple objects for bucket

--- go/aws/s3_bucket/main.go
+++ go/aws/s3_bucket/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
@@ -9,23 +10,34 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
+		// import configuration options
 		config := config.New(ctx, "")
 
+		// get a secret value from the configuration
 		superSecret := config.RequireSecret("supersecret")
 
-		// Create a a private bucket
-		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{Acl: pulumi.String("private")})
+		bucket, err := s3.NewBucket(ctx, "my-bucket", &s3.BucketArgs{
+			Acl: pulumi.String("private"),
+		})
 		if err != nil {
 			return err
 		}
 
-		_, err = s3.NewBucketObject(ctx, "secret", &s3.BucketObjectArgs{
-			Bucket:  bucket.ID(),
-			Key:     pulumi.String("secret"),
-			Content: pulumi.Sprintf("%s", superSecret),
-		})
+		values := []string{"key", "value"}
 
-		ctx.Export("superSecret", superSecret)
+		for _, v := range values {
+			_, err = s3.NewBucketObject(ctx, fmt.Sprintf("object-%s", v), &s3.BucketObjectArgs{
+				Bucket:  bucket.ID(),
+				Content: pulumi.Sprintf("%s", superSecret),
+				Key:     pulumi.String("secret"),
+			})
+			if err != nil {
+				return err
+			}
+		}
+
+
 		return nil
 	})
 }
+
