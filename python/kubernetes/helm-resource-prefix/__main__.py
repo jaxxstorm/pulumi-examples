0235a0913daab248dcbc1409f25a5a27b6503db4
Lee Briggs <lee@leebriggs.co.uk>
2021-05-05 16:16:53 -0700

    add an example of using resource prefix with helm

---
+++ python/kubernetes/helm-resource-prefix/__main__.py
@@ -0,0 +1,34 @@
+"""A Python Pulumi program"""
+
+import pulumi
+import pulumi_kubernetes as k8s
+
+
+# removes the status field from the nginx-ingress crd
+def remove_status(obj):
+    if 'status' in obj:
+        try:
+            del obj["status"]
+        except KeyError:
+            pass
+
+clusters = [
+    "cluster-1",
+    "cluster-2",
+    "cluster-3"
+]
+
+for cluster in clusters:
+    chart = k8s.helm.v3.Chart(
+        "consul",
+        k8s.helm.v3.ChartOpts(
+            resource_prefix=cluster,
+            chart="consul",
+            namespace="default",
+            values={},
+            fetch_opts=k8s.helm.v3.FetchOpts(
+                repo="https://helm.releases.hashicorp.com",
+            ),
+            transformations=[remove_status],
+        ) 
+    )
