e0e0885920c64ef1ca8be484977ee9c23f6cdb43
Lee Briggs <lee@leebriggs.co.uk>
2021-05-17 16:21:01 -0700

    add AKS Go example

---
+++ go/azure/aks-namespace/go.mod
@@ -0,0 +1,12 @@
+module aks-namespace.go
+
+go 1.14
+
+require (
+	github.com/pulumi/pulumi-azure-native/sdk v1.5.0
+	github.com/pulumi/pulumi-azuread/sdk/v4 v4.0.0 // indirect
+	github.com/pulumi/pulumi-kubernetes/sdk/v3 v3.1.2 // indirect
+	github.com/pulumi/pulumi-random/sdk/v4 v4.1.1
+	github.com/pulumi/pulumi-tls/sdk/v4 v4.0.0
+	github.com/pulumi/pulumi/sdk/v3 v3.2.1
+)
---
+++ go/azure/aks-namespace/main.go
@@ -0,0 +1,149 @@
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/pulumi/pulumi-azure-native/sdk/go/azure/containerservice"
+	"github.com/pulumi/pulumi-azure-native/sdk/go/azure/resources"
+	"github.com/pulumi/pulumi-azuread/sdk/v4/go/azuread"
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/providers"
+	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
+	"github.com/pulumi/pulumi-tls/sdk/v4/go/tls"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create an Azure Resource Group
+		resourceGroup, err := resources.NewResourceGroup(ctx, "azure-go-aks", nil)
+		if err != nil {
+			return err
+		}
+
+		// Create an AD service principal.
+		adApp, err := azuread.NewApplication(ctx, "aks", &azuread.ApplicationArgs{
+			DisplayName: pulumi.String("aks"),
+		})
+		if err != nil {
+			return err
+		}
+
+		adSp, err := azuread.NewServicePrincipal(ctx, "aksSp", &azuread.ServicePrincipalArgs{
+			ApplicationId: adApp.ApplicationId,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Generate a random password.
+		password, err := random.NewRandomPassword(ctx, "password", &random.RandomPasswordArgs{
+			Length:  pulumi.Int(20),
+			Special: pulumi.Bool(true),
+		})
+		if err != nil {
+			return err
+		}
+
+		// Create the Service Principal Password.
+		adSpPassword, err := azuread.NewServicePrincipalPassword(ctx, "aksSpPassword", &azuread.ServicePrincipalPasswordArgs{
+			ServicePrincipalId: adSp.ID(),
+			Value:              password.Result,
+			EndDate:            pulumi.String("2099-01-01T00:00:00Z"),
+		})
+		if err != nil {
+			return err
+		}
+
+		// Generate an SSH key.
+		sshArgs := tls.PrivateKeyArgs{
+			Algorithm: pulumi.String("RSA"),
+			RsaBits:   pulumi.Int(4096),
+		}
+		sshKey, err := tls.NewPrivateKey(ctx, "ssh-key", &sshArgs)
+		if err != nil {
+			return err
+		}
+
+		// Create the Azure Kubernetes Service cluster.
+		cluster, err := containerservice.NewManagedCluster(ctx, "go-aks", &containerservice.ManagedClusterArgs{
+			ResourceGroupName: resourceGroup.Name,
+			AgentPoolProfiles: containerservice.ManagedClusterAgentPoolProfileArray{
+				&containerservice.ManagedClusterAgentPoolProfileArgs{
+					Name:         pulumi.String("agentpool"),
+					Mode:         pulumi.String("System"),
+					OsDiskSizeGB: pulumi.Int(30),
+					Count:        pulumi.Int(3),
+					VmSize:       pulumi.String("Standard_DS2_v2"),
+					OsType:       pulumi.String("Linux"),
+				},
+			},
+			LinuxProfile: &containerservice.ContainerServiceLinuxProfileArgs{
+				AdminUsername: pulumi.String("testuser"),
+				Ssh: containerservice.ContainerServiceSshConfigurationArgs{
+					PublicKeys: containerservice.ContainerServiceSshPublicKeyArray{
+						containerservice.ContainerServiceSshPublicKeyArgs{
+							KeyData: sshKey.PublicKeyOpenssh,
+						},
+					},
+				},
+			},
+			DnsPrefix: resourceGroup.Name,
+			ServicePrincipalProfile: &containerservice.ManagedClusterServicePrincipalProfileArgs{
+				ClientId: adApp.ApplicationId,
+				Secret:   adSpPassword.Value,
+			},
+			KubernetesVersion: pulumi.String("1.18.14"),
+		})
+		if err != nil {
+			return err
+		}
+
+		// assign the value of our apply to the kubeConfig variable
+		// this gets resolved async
+		kubeConfig := pulumi.All(cluster.Name, resourceGroup.Name, resourceGroup.ID()).ApplyT(func(args interface{}) (string, error) {
+			clusterName := args.([]interface{})[0].(string)
+			resourceGroupName := args.([]interface{})[1].(string)
+			creds, err := containerservice.ListManagedClusterUserCredentials(ctx, &containerservice.ListManagedClusterUserCredentialsArgs{
+				ResourceGroupName: resourceGroupName,
+				ResourceName:      clusterName,
+			})
+			if err != nil {
+				return "", err
+			}
+			encoded := creds.Kubeconfigs[0].Value
+			kubeconfig, err := base64.StdEncoding.DecodeString(encoded)
+			if err != nil {
+				return "", err
+			}
+			return string(kubeconfig), nil
+		})
+
+		// export the kubeconfig for later user
+		ctx.Export("kubeconfig", kubeConfig)
+
+		// Build a provider with the kubeconfig 
+		k8sProvider, err := providers.NewProvider(ctx, "kubernetes", &providers.ProviderArgs{
+			Kubeconfig: kubeConfig.(pulumi.StringPtrInput),
+		}, pulumi.DependsOn([]pulumi.Resource{cluster}))
+		if err != nil {
+			return fmt.Errorf("error creating provider: %v", err)
+		}
+
+		// Use the provider to build a namespace resource
+		_, err = corev1.NewNamespace(ctx, "app-ns", &corev1.NamespaceArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Name: pulumi.String("test-namespace"),
+			},
+		}, pulumi.Provider(k8sProvider))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
