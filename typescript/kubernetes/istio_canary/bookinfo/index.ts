667ecb1af6cfe363effb5e85aca03d9aa86a861f
Lee Briggs <lee@leebriggs.co.uk>
2022-01-10 11:26:47 -0800

    make canary component

--- typescript/kubernetes/istio_canary/bookinfo/index.ts
+++ typescript/kubernetes/istio_canary/bookinfo/index.ts
@@ -1,7 +1,6 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as i13t from "./istioDeployment";
-import * as istio from "./istio";
-import * as pulumi from "@pulumi/pulumi";
+import * as canary from "./ingressCanary";
 import { v1 } from "@pulumi/kubernetes/types/enums/core";
 
 const ns = new k8s.core.v1.Namespace("bookinfo", {
@@ -19,12 +18,12 @@ const details = new i13t.IstioDeployment(
     image: "docker.io/istio/examples-bookinfo-details-v1:1.16.2",
     port: 9080,
     namespace: ns.metadata.name,
-    extraEnv: [{ name: "details-version", value: "v1" }]
+    extraEnv: [{ name: "details-version", value: "v1" }],
   },
   { parent: ns }
 );
 
-details.createService("details", {port: 9080, namespace: ns.metadata.name })
+details.createService("details", { port: 9080, namespace: ns.metadata.name });
 
 const ratings = new i13t.IstioDeployment(
   "ratings",
@@ -32,13 +31,12 @@ const ratings = new i13t.IstioDeployment(
     image: "docker.io/istio/examples-bookinfo-ratings-v1:1.16.2",
     port: 9080,
     namespace: ns.metadata.name,
-    extraEnv: [{ name: "ratings-version", value: "v1" }]
+    extraEnv: [{ name: "ratings-version", value: "v1" }],
   },
   { parent: ns }
 );
 
-ratings.createService("ratings", {port: 9080, namespace: ns.metadata.name })
-
+ratings.createService("ratings", { port: 9080, namespace: ns.metadata.name });
 
 const reviews = new i13t.IstioDeployment(
   "reviewsv1",
@@ -46,13 +44,12 @@ const reviews = new i13t.IstioDeployment(
     image: "docker.io/istio/examples-bookinfo-reviews-v1:1.16.2",
     port: 9080,
     namespace: ns.metadata.name,
-    extraEnv: [{ name: "reviews-version", value: "v1" }]
+    extraEnv: [{ name: "reviews-version", value: "v1" }],
   },
   { parent: ns }
 );
 
-reviews.createService("reviews", {port: 9080, namespace: ns.metadata.name })
-
+reviews.createService("reviews", { port: 9080, namespace: ns.metadata.name });
 
 const reviewsV2 = new i13t.IstioDeployment(
   "reviewsv2",
@@ -86,120 +83,89 @@ const productPage = new i13t.IstioDeployment(
     image: "docker.io/istio/examples-bookinfo-productpage-v1:1.16.2",
     port: 9080,
     namespace: ns.metadata.name,
-    extraEnv: [{
+    extraEnv: [
+      {
         name: "product-version",
         value: "v1",
-    }]
+      },
+    ],
   },
   { parent: ns }
 );
 
-const productsvc = productPage.createService("productpage", {port: 9080, namespace: ns.metadata.name })
+const productsvc = productPage.createService("productpage", {
+  port: 9080,
+  namespace: ns.metadata.name,
+});
 
-
-const gateway = new istio.networking.v1alpha3.Gateway("bookinfo", {
-  metadata: {
-    namespace: ns.metadata.name,
-    name: "bookinfo-gw",
-  },
-  spec: {
-    selector: {
-      istio: "ingressgateway",
-    },
-    servers: [
-      {
-        port: {
-          number: 80,
-          name: "http",
-          protocol: "HTTP",
+new canary.IngressCanary("bookinfo", {
+  namespace: ns.metadata.name,
+  host: productsvc.metadata.name,
+  http: [
+    {
+      match: [
+        {
+          uri: {
+            exact: "/productpage",
+          },
         },
-        hosts: ["*"],
-      },
-    ],
-  },
-}, { parent: ns });
-
-const virtualSvc = new istio.networking.v1alpha3.VirtualService("bookinfo", {
-  metadata: {
-    namespace: ns.metadata.name,
-  },
-  spec: {
-    hosts: ["*"],
-    gateways: ["bookinfo-gw"],
-    http: [
-      {
-        match: [
-          {
-            uri: {
-              exact: "/productpage",
+        {
+          uri: {
+            prefix: "/static",
+          },
+        },
+        {
+          uri: {
+            exact: "/login",
+          },
+        },
+        {
+          uri: {
+            exact: "/logout",
+          },
+        },
+        {
+          uri: {
+            prefix: "/api/v1/products",
+          },
+        },
+      ],
+      route: [
+        {
+          destination: {
+            host: productsvc.metadata.name,
+            subset: "v1",
+            port: {
+              number: 9080,
             },
           },
-          {
-            uri: {
-              prefix: "/static",
+          weight: 90,
+        },
+        {
+          destination: {
+            host: productsvc.metadata.name,
+            subset: "v2",
+            port: {
+              number: 9080,
             },
           },
-          {
-            uri: {
-              exact: "/login",
-            },
-          },
-          {
-            uri: {
-              exact: "/logout",
-            },
-          },
-          {
-            uri: {
-              prefix: "/api/v1/products",
-            },
-          },
-        ],
-        route: [
-          {
-            destination: {
-              host: productsvc.metadata.name,
-              subset: "v1",
-              port: {
-                number: 9080,
-              },
-            },
-            weight: 90,
-          },
-          {
-            destination: {
-              host: productsvc.metadata.name,
-              subset: "v2",
-              port: {
-                number: 9080,
-              },
-            },
-            weight: 10,
-          }
-          
-        ],
-      },
-    ],
-  },
-}, { parent: ns });
-
-const destination = new istio.networking.v1alpha3.DestinationRule("canary", {
-  metadata: {
-    namespace: ns.metadata.name
-  },
-  spec: {
-    host: productsvc.metadata.name,
-    subsets: [{
+          weight: 10,
+        },
+      ],
+    },
+  ],
+  subsets: [
+    {
       name: "v1",
       labels: {
         version: "v1",
-      }
-    }, {
+      },
+    },
+    {
       name: "v2",
       labels: {
         version: "v2",
-      }
-    }]
-    
-  }
-}, { parent: ns})
+      },
+    },
+  ],
+});
--- typescript/kubernetes/istio_canary/bookinfo/ingressCanary.ts
+++ typescript/kubernetes/istio_canary/bookinfo/ingressCanary.ts
@@ -1,10 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as k8s from "@pulumi/kubernetes";
+import * as istio from "./istio";
 
-export interface IngressCanaryArgs {}
+export interface IngressCanaryArgs {
+    namespace: pulumi.Input<string>;
+    istioSelector?: pulumi.Input<string>;
+    host: pulumi.Input<string>;
+    http: pulumi.Input<pulumi.Input<istio.types.input.networking.v1alpha3.VirtualServiceSpecHttpArgs>[]>;
+    subsets: pulumi.Input<pulumi.Input<istio.types.input.networking.v1alpha3.DestinationRuleSpecSubsetsArgs>[]>;
+}
 
 export class IngressCanary extends pulumi.ComponentResource {
   
+    gateway: istio.networking.v1alpha3.Gateway;
+    virtualService: istio.networking.v1alpha3.VirtualService;
+    destination: istio.networking.v1alpha3.DestinationRule
     private readonly name: string;
   
     constructor(
@@ -15,5 +24,51 @@ export class IngressCanary extends pulumi.ComponentResource {
       super("jaxxstorm:index:IngressCanary", name, {}, opts);
   
       this.name = name;
+
+      this.gateway = new istio.networking.v1alpha3.Gateway(name, {
+        metadata: {
+          namespace: args.namespace,
+          name: name,
+        },
+        spec: {
+          selector: {
+            istio: args.istioSelector || "ingressgateway",
+          },
+          servers: [
+            {
+              port: {
+                number: 80,
+                name: "http",
+                protocol: "HTTP",
+              },
+              hosts: ["*"],
+            },
+          ],
+        },
+      }, { parent: this });
+
+      this.virtualService = new istio.networking.v1alpha3.VirtualService(name, {
+        metadata: {
+          namespace: args.namespace,
+        },
+        spec: {
+          hosts: ["*"],
+          gateways: [ name ] ,
+          http: args.http,
+        },
+      }, { parent: this });
+
+      this.destination = new istio.networking.v1alpha3.DestinationRule(name, {
+        metadata: {
+          namespace: args.namespace
+        },
+        spec: {
+          host: args.host,
+          subsets: args.subsets,
+          
+        }
+      }, { parent: this })
+      
+
     }
 }
