867f2f2c55b35ec98b775668a85aead175f7b373
Lee Briggs <lee@leebriggs.co.uk>
2022-02-08 12:57:22 -0800

    add s3 bucket example

--- python/aws/cloudformation_template/__main__.py
+++ python/aws/cloudformation_template/__main__.py
@@ -4,29 +4,27 @@ import pulumi
 import pulumi_aws as aws
 import pulumi_tls as tls
 
-ssh_key = tls.PrivateKey("generated", 
+ssh_key = tls.PrivateKey(
+    "generated",
     algorithm="RSA",
     rsa_bits=4096,
 )
 
-aws_key = aws.ec2.KeyPair("generated",
+aws_key = aws.ec2.KeyPair(
+    "generated",
     public_key=ssh_key.public_key_openssh,
-    opts=pulumi.ResourceOptions(parent=ssh_key)
+    opts=pulumi.ResourceOptions(parent=ssh_key),
 )
 
-ec2_instance = aws.cloudformation.Stack("ec2-instance",
+ec2_instance = aws.cloudformation.Stack(
+    "ec2-instance",
     template_url="https://s3.us-west-2.amazonaws.com/cloudformation-templates-us-west-2/EC2InstanceWithSecurityGroupSample.template",
-    parameters={
-        "KeyName": aws_key.key_name
-    }
+    parameters={"KeyName": aws_key.key_name},
 )
 
-cluster = aws.cloudformation.get_stack(
-    name="lbriggs-87c98b5e"
-)
+cluster = aws.cloudformation.get_stack(name="lbriggs-87c98b5e")
 
 asg = aws.autoscaling.get_group(name=cluster.outputs["NodeGroup"])
-
 
 
 pulumi.export("address", ec2_instance.outputs["PublicDNS"])
---
+++ python/aws/s3_bucket_cloudflare/__main__.py
@@ -0,0 +1,92 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+import json
+import os
+import mimetypes
+import pulumi_aws as aws
+import pulumi_cloudflare as cloudflare
+
+bucket = aws.s3.Bucket(
+    "static.briggs.work",
+    bucket="static.briggs.work",
+    website=aws.s3.BucketWebsiteArgs(
+        index_document="index.html",
+        error_document="404.html"
+    ),
+    opts=pulumi.ResourceOptions(delete_before_replace=True)
+)
+
+content_dir = "www"
+for file in os.listdir(content_dir):
+    filepath = os.path.join(content_dir, file)
+    mime_type, _ = mimetypes.guess_type(filepath)
+    obj = aws.s3.BucketObject(file,
+        bucket=bucket.id,
+        source=pulumi.FileAsset(filepath),
+        content_type=mime_type)
+
+
+
+def allow_cloudflare_ips(bucket_arn):
+    return json.dumps(
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "PublicReadGetObject",
+                    "Effect": "Allow",
+                    "Principal": "*",
+                    "Action": "s3:GetObject",
+                    "Resource": f"{bucket_arn}/*",
+                    "Condition": {
+                        "IpAddress": {
+                            "aws:SourceIp": [
+                                "2400:cb00::/32",
+                                "2606:4700::/32",
+                                "2803:f800::/32",
+                                "2405:b500::/32",
+                                "2405:8100::/32",
+                                "2a06:98c0::/29",
+                                "2c0f:f248::/32",
+                                "173.245.48.0/20",
+                                "103.21.244.0/22",
+                                "103.22.200.0/22",
+                                "103.31.4.0/22",
+                                "141.101.64.0/18",
+                                "108.162.192.0/18",
+                                "190.93.240.0/20",
+                                "188.114.96.0/20",
+                                "197.234.240.0/22",
+                                "198.41.128.0/17",
+                                "162.158.0.0/15",
+                                "172.64.0.0/13",
+                                "131.0.72.0/22",
+                                "104.16.0.0/13",
+                                "104.24.0.0/14",
+                            ]
+                        }
+                    },
+                }
+            ],
+        }
+    )
+
+
+bucketPolicy = aws.s3.BucketPolicy(
+    "allowCloudflare",
+    bucket=bucket.bucket,
+    policy=bucket.arn.apply(allow_cloudflare_ips),
+    opts=pulumi.ResourceOptions(parent=bucket),
+)
+
+zone = cloudflare.get_zone(name="briggs.work")
+
+record = cloudflare.Record(
+    "static",
+    zone_id=zone.id,
+    name="static",
+    value=bucket.website_endpoint,
+    type="CNAME",
+    ttl=3600,
+)
