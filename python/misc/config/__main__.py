5d9dbb9c12b94e89a614aad3a7a5e04cc2943fc1
Lee Briggs <lee@leebriggs.co.uk>
2022-03-30 14:31:15 -0700

    update secret

--- python/misc/config/__main__.py
+++ python/misc/config/__main__.py
@@ -1,17 +1,18 @@
 """A Python Pulumi program"""
 
 import pulumi
+import json
+
 config = pulumi.Config()
 foo = config.require_object("foo")
 secret_foo = config.require_secret_object("foo")
 allowed_hosts = foo.get("allowed_hosts")
-secret_allowed_hosts = secret_foo.apply(need_list)
-print('type of not secret',type(allowed_hosts))
 pulumi.log.info(f'h1 {allowed_hosts[0]}')
 pulumi.log.info(f'h2 {allowed_hosts[1]}')
 secret_foo.apply(lambda host: print(host))
-# pulumi.log.info(secret_allowed_hosts[2])  # this line is broken and produces error
 
+cfg = pulumi.Config("access")
+users = cfg.get_object("users")
+users = json.loads(cfg.get("users"))
 
-
-print(result)
+print(users)
