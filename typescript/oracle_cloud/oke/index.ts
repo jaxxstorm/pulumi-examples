85075d375be7ef4e6208a2dfa032ae77f4fef862
Lee Briggs <lee@leebriggs.co.uk>
2022-10-13 12:59:55 -0700

    add oke example

---
+++ typescript/oracle_cloud/oke/index.ts
@@ -0,0 +1,130 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as vcn from "./vcn";
+import * as oci from "@pulumi/oci";
+import * as init from "@pulumi/cloudinit";
+
+const compartment = oci.identity.getCompartmentOutput({
+  id: "ocid1.tenancy.oc1..aaaaaaaavh67gfytloujvijdue6pqwhfrlo2ms4jbkpozyez7sgdhf27e4yq",
+});
+
+const okeVcn = new vcn.OciVcn("example", {
+  cidrBlock: "172.16.0.0/22",
+  compartmentId: compartment.id,
+  createInternetGateway: true,
+  createNatGateway: true,
+  createServiceGateway: true,
+});
+
+const kubeVersion = "v1.24.1";
+
+const cluster = new oci.containerengine.Cluster("example", {
+  compartmentId: compartment.id,
+  vcnId: okeVcn.vcn.id,
+  kubernetesVersion: kubeVersion,
+  options: {
+    serviceLbSubnetIds: okeVcn.publicSubnetIds,
+  },
+  endpointConfig: {
+    isPublicIpEnabled: true,
+    subnetId: okeVcn.publicSubnetIds[0],
+  },
+});
+
+const sl = new oci.core.SecurityList("example", {
+  compartmentId: compartment.id,
+  vcnId: okeVcn.vcn.id,
+  egressSecurityRules: [
+    {
+      destination: "0.0.0.0/0",
+      protocol: "all",
+      stateless: false,
+      description: "allow all traffic to the internet",
+    },
+  ],
+  ingressSecurityRules: [
+    {
+      description: "allow all traffic inside the VCN",
+      protocol: "all",
+      stateless: false,
+      source: okeVcn.vcn.cidrBlock,
+    },
+    {
+      description: "allow access to OKE control plane",
+      protocol: "6",
+      stateless: false,
+      source: "0.0.0.0/0",
+    },
+  ],
+});
+
+const kubeconfigOutput = oci.containerengine.getClusterKubeConfigOutput({
+  clusterId: cluster.id,
+});
+
+export const kubeconfig = kubeconfigOutput.content;
+
+let availDomains = oci.identity.getAvailabilityDomainsOutput({
+  compartmentId: compartment.id,
+});
+
+export const availabilityDomains = availDomains.availabilityDomains;
+
+const imageId = "ocid1.image.oc1.phx.aaaaaaaaxmrdfmxqn2vdm5fawepynsgklnthansysonqkvr2odhzxx5w4awa";
+
+const workerUserData = init.getConfigOutput({
+    gzip: false,
+    base64Encode: true,
+    parts: [{
+        filename: "worker.sh",
+        contentType: "text/x-shellscript",
+        content: `
+        #!/bin/bash
+
+# DO NOT MODIFY
+curl --fail -H "Authorization: Bearer Oracle" -L0 http://169.254.169.254/opc/v2/instance/metadata/oke_init_script | base64 --decode >/var/run/oke-init.sh
+
+## run oke provisioning script
+bash -x /var/run/oke-init.sh
+
+### adjust block volume size
+/usr/libexec/oci-growfs -y
+
+touch /var/log/oke.done`
+    }]
+})
+
+// const pool1 = new oci.containerengine.NodePool("pool1", {
+//     clusterId: cluster.id,
+//     compartmentId: compartment.id,
+//     initialNodeLabels: [{
+//         key: "name",
+//         value: "pool1",
+//     }],
+//     kubernetesVersion: kubeVersion,
+//     name: "pool1",
+//     nodeConfigDetails: {
+//         nodePoolPodNetworkOptionDetails: {
+//             cniType: "FLANNEL_OVERLAY",
+//         },
+//         placementConfigs: [{
+//             availabilityDomain: availabilityDomains[0].name,
+//             subnetId: okeVcn.privateSubnetIds[0],
+//         }],
+//         size: 1,
+//     },
+//     nodeEvictionNodePoolSettings: {
+//         evictionGraceDuration: "PT1H",
+//     },
+//     nodeShape: "VM.Standard.E3.Flex",
+//     nodeShapeConfig: {
+//         memoryInGbs: 16,
+//         ocpus: 1,
+//     },
+//     nodeSourceDetails: {
+//         imageId: imageId,
+//         sourceType: "IMAGE",
+//     },
+//     nodeMetadata: {
+//         user_data: workerUserData.rendered,
+//     }
+// });
---
+++ typescript/oracle_cloud/oke/vcn.ts
@@ -0,0 +1,182 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as oci from "@pulumi/oci";
+import { SubnetDistributor } from "./subnetDistributor";
+
+export interface OciVcnAppArgs {
+  compartmentId: pulumi.Output<string>;
+  cidrBlock: string;
+  dnsLabel?: pulumi.Input<string>;
+  isIpv6enabled?: pulumi.Input<boolean>;
+  createInternetGateway?: boolean;
+  createNatGateway?: boolean;
+  createServiceGateway?: boolean;
+}
+
+export class OciVcn extends pulumi.ComponentResource {
+  vcn: oci.core.Vcn;
+  igw?: oci.core.InternetGateway;
+  natGateway?: oci.core.NatGateway;
+  serviceGateway?: oci.core.ServiceGateway;
+  publicSubnets: oci.core.Subnet[] = [];
+  privateSubnets: oci.core.Subnet[] = [];
+  igwRoute?: oci.core.RouteTable;
+  natGatewayRoute?: oci.core.RouteTable;
+  services?: Promise<oci.core.GetServicesResult>;
+  publicSubnetIds: any[] = [];
+  privateSubnetIds: any[] = [];
+
+  constructor(
+    name: string,
+    args: OciVcnAppArgs,
+    opts?: pulumi.ComponentResourceOptions
+  ) {
+    super("jaxxstorm:index:OciVcn", name, {}, opts);
+
+    this.vcn = new oci.core.Vcn(
+      name,
+      {
+        compartmentId: args.compartmentId,
+        cidrBlocks: [args.cidrBlock],
+        dnsLabel: args.dnsLabel,
+        isIpv6enabled: args.isIpv6enabled,
+        
+      },
+      { parent: this }
+    );
+
+    // define some subnets
+    const distributor = new SubnetDistributor(args.cidrBlock, 1); // oracle cloud subnets are regional, so only create 1
+
+    this.publicSubnets = distributor.publicSubnets().map((cidr, index) => {
+
+      let subnet = new oci.core.Subnet(
+        `${name}-public-${index + 1}`,
+        {
+          vcnId: this.vcn.id,
+          compartmentId: args.compartmentId,
+          cidrBlock: cidr,
+          prohibitPublicIpOnVnic: false,
+        },
+        { parent: this.vcn }
+      );
+
+      this.publicSubnetIds.push(subnet.id)
+
+      return subnet
+
+
+    });
+
+    this.privateSubnets = distributor.privateSubnets().map((cidr, index) => {
+
+      let subnet = new oci.core.Subnet(
+        `${name}-private-${index + 1}`,
+        {
+          vcnId: this.vcn.id,
+          compartmentId: args.compartmentId,
+          cidrBlock: cidr,
+          prohibitPublicIpOnVnic: true,
+        },
+        { parent: this.vcn }
+      );
+
+      this.privateSubnetIds.push(subnet.id)
+
+      return subnet
+      
+    });
+
+    // we're asking for an internet gateway
+    if (args.createInternetGateway) {
+      this.igw = new oci.core.InternetGateway(
+        name,
+        {
+          compartmentId: args.compartmentId,
+          vcnId: this.vcn.id,
+        },
+        { parent: this.vcn }
+      );
+
+      this.igwRoute = new oci.core.RouteTable(
+        name,
+        {
+          vcnId: this.vcn.id,
+          compartmentId: args.compartmentId,
+          routeRules: [
+            {
+              destination: "0.0.0.0/0",
+              networkEntityId: this.igw.id,
+            },
+          ],
+        },
+        { parent: this.igw }
+      );
+    }
+
+    if (args.createServiceGateway) {
+      this.services = oci.core.getServices({
+        filters: [
+          {
+            name: "name",
+            values: ["All .* Services In Oracle Services Network"],
+            regex: true,
+          },
+        ],
+      });
+
+      this.serviceGateway = new oci.core.ServiceGateway(name, {
+        vcnId: this.vcn.id,
+        compartmentId: args.compartmentId,
+        services: [
+          {
+            serviceId: this.services.then(svc => svc.services[0].id),
+          },
+        ],
+      }, { parent: this.vcn, deleteBeforeReplace: true });
+    }
+
+    // we want a nat gateway
+    if (args.createNatGateway) {
+      this.natGateway = new oci.core.NatGateway(
+        name,
+        {
+          compartmentId: args.compartmentId,
+          vcnId: this.vcn.id,
+        },
+        { parent: this.vcn }
+      );
+
+      const routeRules: pulumi.Input<
+        pulumi.Input<oci.types.input.Core.RouteTableRouteRule>[]
+      > = [];
+
+      const defaultRouteRules = {
+        networkEntityId: this.natGateway.id,
+        destination: "0.0.0.0/0",
+        destinationType: "CIDR_BLOCK",
+        description: "Created by Pulumi",
+      };
+
+      routeRules.push(defaultRouteRules);
+
+      if (args.createServiceGateway) {
+        routeRules.push({
+          destinationType: "SERVICE_CIDR_BLOCK",
+          destination: this.services?.then((s) => s.services[0].cidrBlock),
+          networkEntityId: this.serviceGateway!.id,
+          description: "Created by Pulumi",
+        });
+      }
+
+      this.natGatewayRoute = new oci.core.RouteTable(
+        name,
+        {
+          vcnId: this.vcn.id,
+          compartmentId: args.compartmentId,
+          routeRules: routeRules,
+        },
+        { parent: this.natGateway }
+      );
+    }
+  }
+}
